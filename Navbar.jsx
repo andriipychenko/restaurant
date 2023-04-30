76f698102eca5ef2118e9be8431ca2e1a541553f
andrii/ <129830244+andriipychenko@users.noreply.github.com>
2023-04-30 23:38:53 +0200

    Add files via upload

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,6 +1,3 @@
-
-
-
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
@@ -46,3 +43,35 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// povna chastina
+
+
+// import React from 'react';
+// import { GiHamburgerMenu } from 'react-icons/gi';
+// import { MdOutlineRestaurantMenu } from 'react-icons/md';
+// import './Navbar.css';
+// import images from '../../constants/images'
+
+// const Navbar = () => (
+//   <nav className='app__navbar'>
+//     <div className='app__navbar-logo'>
+//       <img src={images.gericht} alt="app logo" />
+//     </div>
+//     <ul className='app__navbar-links'>
+//       <li className='p__opensans'> <a href="#home">Home</a> </li>
+//       <li className='p__opensans'><a href="#about">About</a> </li>
+//       <li className='p__opensans'><a href="#menu">Menu</a> </li>
+//       <li className='p__opensans'><a href="#awards">Awards</a> </li>
+//       <li className='p__opensans'><a href="#contact">Contact</a> </li>
+//     </ul>
+//     <div className='app__navbar-login'>
+//       <a href="#login" className='p__opensans'>Log In / Register</a>
+//       <div />
+//       <a href="/" className='p__opensans'>Book Table</a>
+//     </div>
+
+//   </nav>
+// );
+
+// export default Navbar;

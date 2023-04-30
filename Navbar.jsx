69cb3b0c83c68777c30da6fe6faedf4ef2f9650f
andrii/ <129830244+andriipychenko@users.noreply.github.com>
2023-04-30 23:54:19 +0200

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,16 +1,29 @@
+//https://react-icons.github.io/react-icons/
+
+
 import React from 'react';
+import './Navbar.css';
+
+import images from '../../constants/images';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
-import images from '../../constants/images';
-import './Navbar.css';
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
+  
   return (
     <nav className="app__navbar">
+    
+      //1.BRAND
       <div className="app__navbar-logo">
         <img src={images.gericht} alt="app__logo" />
       </div>
+      
+      
+      
+      
+      
+      //2.LINKS
       <ul className="app__navbar-links">
         <li className="p__opensans"><a href="#home">Home</a></li>
         <li className="p__opensans"><a href="#about">About</a></li>
@@ -23,8 +36,11 @@ const Navbar = () => {
         <div />
         <a href="/" className="p__opensans">Book Table</a>
       </div>
+      
+      //3.TOGGLE MENU
       <div className="app__navbar-smallscreen">
         <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        
         {toggleMenu && (
           <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
             <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
@@ -37,6 +53,7 @@ const Navbar = () => {
             </ul>
           </div>
         )}
+        
       </div>
     </nav>
   );
@@ -44,34 +61,3 @@ const Navbar = () => {
 
 export default Navbar;
 
-// povna chastina
-
-
-// import React from 'react';
-// import { GiHamburgerMenu } from 'react-icons/gi';
-// import { MdOutlineRestaurantMenu } from 'react-icons/md';
-// import './Navbar.css';
-// import images from '../../constants/images'
-
-// const Navbar = () => (
-//   <nav className='app__navbar'>
-//     <div className='app__navbar-logo'>
-//       <img src={images.gericht} alt="app logo" />
-//     </div>
-//     <ul className='app__navbar-links'>
-//       <li className='p__opensans'> <a href="#home">Home</a> </li>
-//       <li className='p__opensans'><a href="#about">About</a> </li>
-//       <li className='p__opensans'><a href="#menu">Menu</a> </li>
-//       <li className='p__opensans'><a href="#awards">Awards</a> </li>
-//       <li className='p__opensans'><a href="#contact">Contact</a> </li>
-//     </ul>
-//     <div className='app__navbar-login'>
-//       <a href="#login" className='p__opensans'>Log In / Register</a>
-//       <div />
-//       <a href="/" className='p__opensans'>Book Table</a>
-//     </div>
-
-//   </nav>
-// );
-
-// export default Navbar;
